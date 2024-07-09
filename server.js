fa9c3def77685c0a0a187b1bd764f1a1231cc95f
BigcatSolu <167731075+BigcatSolu@users.noreply.github.com>
2024-07-10 00:58:16 +0700

    Update and rename index.js to server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser())
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../client/public/upload')
+        cb(null, './public/upload')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + file.originalname);
@@ -34,6 +34,4 @@ app.use("/api/users", usersRoutes)
 app.use("/api/auth", authRoutes)
 
 
-app.listen(3006, ()=> {
-    console.log('Connected')
-})
+export default app;
